ebbd0241cd9c89eac12afd6457570f3236a90cbd
Nethelbert <gilblasse@gmail.com>
2019-12-14 03:24:05 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -92,7 +92,7 @@ end
 
 def can_destroy_all_items_at_once
   10.times do |i|
-    Movie.create(title: "Movie_#{i}").tap{|m| m.destroy}
+    Movie.create(title: "Movie_#{i}")
   end
   Movies.destroy_all
 end
