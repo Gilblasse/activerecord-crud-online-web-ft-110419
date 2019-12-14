3ff481d49f65e2082f36d555ff1c52bd5115a968
Nethelbert <gilblasse@gmail.com>
2019-12-14 03:20:53 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -88,8 +88,6 @@ end
 
 def can_destroy_a_single_item
   Movie.create(title: "That One Where the Guy Kicks Another Guy Once").tap{|m| m.destroy}
-  __
-  __
 end
 
 def can_destroy_all_items_at_once
